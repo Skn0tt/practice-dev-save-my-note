fca08ee555063f15d53a16cd5ae9100e47b95097
Simon Knott <info@simonknott.de>
2020-07-22 10:12:10 +0200

    Add saving/loading behaviour to UI

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,15 +1,44 @@
+import { useState } from "react"
+import saveNote from "app/mutations/saveNote"
+import loadNote from "app/queries/loadNote"
+
 function Home() {
+  const [note, setNote] = useState("")
+  const [loading, setLoading] = useState(false)
   return (
     <div className="main-content">
       <h1>Save my note</h1>
       <div className="textbox" data-test="note">
-        <input placeholder="Enter note..." type="text" />
+        <input
+          placeholder="Enter note..."
+          type="text"
+          value={note}
+          onChange={(evt) => setNote(evt.target.value)}
+        />
       </div>
       <div className="buttons">
-        <button data-test="save-btn" className="btn btn-primary btn-save">
+        <button
+          data-test="save-btn"
+          className="btn btn-primary btn-save"
+          disabled={loading}
+          onClick={async () => {
+            setLoading(true)
+            await saveNote(note)
+            setLoading(false)
+          }}
+        >
           Save
         </button>
-        <button data-test="load-btn" className="btn btn-primary btn-load">
+        <button
+          data-test="load-btn"
+          className="btn btn-primary btn-load"
+          disabled={loading}
+          onClick={async () => {
+            setLoading(true)
+            setNote(await loadNote({}))
+            setLoading(false)
+          }}
+        >
           Load
         </button>
       </div>
--- app/queries/loadNote.ts
+++ app/queries/loadNote.ts
@@ -1,6 +1,6 @@
 import db from "db"
 
-export default async function loadNote(): Promise<string> {
+export default async function loadNote({}: any): Promise<string> {
   const result = await db.notes.findOne({ where: { id: 1 }, select: { content: true } })
   return result?.content ?? ""
 }
